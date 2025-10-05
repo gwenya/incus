1b11db2c564b219f490c3e189441df5b6d560177
Gwendolyn <me@gwendolyn.dev>
2025-10-05 13:33:29 +0200

    bpf token test
    Signed-off-by: Gwendolyn <me@gwendolyn.dev>

--- test/suites/container_bpf_tokens.sh
+++ test/suites/container_bpf_tokens.sh
@@ -1,89 +1,95 @@
-ensure_import_bpf_testimage() {
-  if ! incus image alias list | grep -q "^| testimage-bpf\\s*|.*$"; then
-      if [ -e "${INCUS_BPF_TEST_IMAGE:-}" ]; then
-          incus image import "${INCUS_BPF_TEST_IMAGE}" --alias testimage-bpf
-      else
-        ensure_import_testimage
-        old_dir="$(pwd)"
+get_static_bpf_tool() {
+  if [ -e "${INCUS_BPFTOOL_STATIC_BINARY:-}" ]; then
+    echo "$INCUS_BPFTOOL_STATIC_BINARY"
+  else
+    archive_path=$(mktemp -p "${TEST_DIR}" bpftool-XXX.tar.xz)
+    unpack_path=$(mktemp -p "${TEST_DIR}" bpftool-XXX)
 
-        bpftool_dir=$(mktemp -d -p "${TEST_DIR}" bpftool-XXX)
-        image_dir=$(mktemp -d -p "${TEST_DIR}" bpf-testimage-XXX)
-        image_file=$(mktemp -p "${TEST_DIR}" bpf-testimage-XXX.tar.xz)
-        testimage_file=$(mktemp -p "${TEST_DIR}" testimage-XXX)
+    curl -L -o "$archive_path" https://github.com/libbpf/bpftool/releases/download/v7.6.0/bpftool-v7.6.0-amd64.tar.gz
 
-        mkdir -p "$bpftool_dir" "$image_dir"
+    tar xf "$archive_path" -C "$unpack_path"
 
-        incus image export testimage "$testimage_file"
-        testimage_file="${testimage_file}.tar.xz"
-
-        tar xf "$testimage_file" -C "$image_dir"
-
-        git clone --depth=1 --branch=v7.6.0 --recurse-submodules https://github.com/libbpf/bpftool "$bpftool_dir"
-        cd "$bpftool_dir/src" || return 1
-
-        EXTRA_LDFLAGS=-static make
-
-        cp "bpftool" "$image_dir/rootfs/bin/bpftool"
-
-        arch="$(uname -m)"
-        now="$(date +%s)"
-
-        cat > "$image_dir/metadata.yaml" <<EOF
-architecture: $arch
-creation_date: $now
-properties:
-  description: Bpftool $arch
-EOF
-
-        cd "$image_dir" || return 1
-        tar cJf "$image_file" .
-
-        incus image import "$image_file" --alias testimage-bpf
-
-        cd "$old_dir" || return 1
-        rm -rf "$bpftool_dir" "$image_dir" "$image_file" "$testimage_file"
-      fi
+    echo "$unpack_path/bpftool"
   fi
 }
 
 
 test_container_bpf_token() {
-    ensure_import_bpf_testimage
+  ensure_import_testimage
 
-    incus launch testimage-bpf foo \
-      security.bpffs.delegate_cmds=map_create,prog_attach \
-      security.bpffs.delegate_maps=hash,array \
-      security.bpffs.delegate_progs=socket_filter,xdp,kprobe \
-      security.bpffs.delegate_attachs=cgroup_inet_ingress,sk_skb_stream_parser
+  incus init testimage foo
 
-    bpftool_output="$(incus exec foo -- bpftool --json token list  | jq --sort-keys)"
-    bpftool_desired_output='
-[
-  {
-    "token_info": "/sys/fs/bpf",
-    "allowed_cmds": [
-      "map_create",
-      "prog_attach"
-    ],
-    "allowed_maps": [
-      "hash",
-      "array"
-    ],
-    "allowed_progs": [
-      "socket_filter",
-      "kprobe",
-      "xdp"
-    ],
-    "allowed_attachs": [
-      "cgroup_inet_ingress",
-      "sk_skb_stream_parser"
-    ]
-  }
-]
-    '
-    bpftool_desired_output=$(echo "$bpftool_desired_output" | jq --sort-keys)
+  bpftool_path=$(get_static_bpf_tool)
 
-    diff <(echo "$bpftool_output") <(echo "$bpftool_desired_output") > /dev/null
+  incus file push "$bpftool_path" foo/bin/bpftool
 
-    incus delete -f foo
+  test_container_bpf_token_delegate
+  test_container_bpf_token_path
+
+  incus delete -f foo
+}
+
+
+test_container_bpf_token_delegate() {
+      incus config set foo security.bpffs.delegate_cmds=map_create,prog_attach
+      incus config set foo security.bpffs.delegate_maps=hash,array
+      incus config set foo security.bpffs.delegate_progs=socket_filter,xdp,kprobe
+      incus config set foo security.bpffs.delegate_attachs=cgroup_inet_ingress,sk_skb_stream_parser
+
+      bpftool_output="$(incus exec foo -- /bin/bpftool --json token list  | jq --sort-keys)"
+      bpftool_desired_output='
+  [
+    {
+      "token_info": "/sys/fs/bpf",
+      "allowed_cmds": [
+        "map_create",
+        "prog_attach"
+      ],
+      "allowed_maps": [
+        "hash",
+        "array"
+      ],
+      "allowed_progs": [
+        "socket_filter",
+        "kprobe",
+        "xdp"
+      ],
+      "allowed_attachs": [
+        "cgroup_inet_ingress",
+        "sk_skb_stream_parser"
+      ]
+    }
+  ]
+      '
+      bpftool_desired_output=$(echo "$bpftool_desired_output" | jq --sort-keys)
+
+      test "$bpftool_output" = "$bpftool_desired_output"
+}
+
+test_container_bpf_token_path() {
+      incus config set foo security.bpffs.path=/bpffs
+
+      # we need to enable one of the delegate settings to enable the token
+      incus config set foo security.bpffs.delegate_cmds=map_create
+      incus config unset foo security.bpffs.delegate_attachs
+      incus config unset foo security.bpffs.delegate_maps
+      incus config unset foo security.bpffs.delegate_progs
+
+      bpftool_output="$(incus exec foo -- bpftool --json token list  | jq --sort-keys)"
+      bpftool_desired_output='
+  [
+    {
+      "token_info": "/bpffs",
+      "allowed_cmds": [
+        "map_create"
+      ],
+      "allowed_maps": [],
+      "allowed_progs": [],
+      "allowed_attachs": []
+    }
+  ]
+      '
+      bpftool_desired_output=$(echo "$bpftool_desired_output" | jq --sort-keys)
+
+      test "$bpftool_output" = "$bpftool_desired_output"
 }
