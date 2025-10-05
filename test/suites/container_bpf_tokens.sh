cc621cbf180bd036a41a1b0abaf3dd3e0b3f18ca
Gwendolyn <me@gwendolyn.dev>
2025-10-05 14:40:43 +0200

    bpf token test
    Signed-off-by: Gwendolyn <me@gwendolyn.dev>

--- test/suites/container_bpf_tokens.sh
+++ test/suites/container_bpf_tokens.sh
@@ -37,82 +37,73 @@ test_container_bpf_token() {
   incus exec foo -- chmod +x /bin/bpftool
   incus stop foo
 
-  test_container_bpf_token_delegate
-
-  unset_bpffs_config
-
-  test_container_bpf_token_path
+  bpf_token_test "" "map_create,prog_attach" "hash,array" "socket_filter,xdp,kprobe" "cgroup_inet_ingress,sk_skb_stream_parser"
+  bpf_token_test "" "any" "any" "any" "any"
+  bpf_token_test "" "map_create" "" "" ""
+  bpf_token_test "" "" "hash" "" ""
+  bpf_token_test "" "" "" "socket_filter" ""
+  bpf_token_test "" "" "" "" "cgroup_inet_ingress"
+  bpf_token_test "/mnt" "map_create" "" "" ""
 
   incus delete -f foo
 }
 
+bpf_token_test() {
+  path="$1"
+  cmds="$2"
+  maps="$3"
+  progs="$4"
+  attachs="$5"
 
-test_container_bpf_token_delegate() {
-  incus config set foo security.bpffs.delegate_cmds=map_create,prog_attach
-  incus config set foo security.bpffs.delegate_maps=hash,array
-  incus config set foo security.bpffs.delegate_progs=socket_filter,xdp,kprobe
-  incus config set foo security.bpffs.delegate_attachs=cgroup_inet_ingress,sk_skb_stream_parser
+  if [ "$path" != "" ]; then
+    incus config set foo security.bpffs.path="$path"
+  else
+    incus config unset security.bpffs.path || true
+  fi
+
+  if [ "$cmds" != "" ]; then
+    incus config set foo security.bpffs.delegate_cmds="$cmds"
+  else
+    incus config unset security.bpffs.delegate_cmds || true
+  fi
+
+  if [ "$maps" != "" ]; then
+    incus config set foo security.bpffs.delegate_maps="$maps"
+  else
+    incus config unset security.bpffs.delegate_maps || true
+  fi
+
+  if [ "$progs" != "" ]; then
+    incus config set foo security.bpffs.delegate_progs="$progs"
+  else
+    incus config unset security.bpffs.delegate_progs || true
+  fi
+
+  if [ "$attachs" != "" ]; then
+    incus config set foo security.bpffs.delegate_attachs="$attachs"
+  else
+    incus config unset security.bpffs.delegate_attachs || true
+  fi
 
   incus start foo
   bpftool_output="$(incus exec foo -- /bin/bpftool --json token list  | jq --sort-keys)"
   incus stop foo
 
-  bpftool_desired_output='
-  [
-    {
-      "token_info": "/sys/fs/bpf",
-      "allowed_cmds": [
-        "map_create",
-        "prog_attach"
-      ],
-      "allowed_maps": [
-        "hash",
-        "array"
-      ],
-      "allowed_progs": [
-        "socket_filter",
-        "kprobe",
-        "xdp"
-      ],
-      "allowed_attachs": [
-        "cgroup_inet_ingress",
-        "sk_skb_stream_parser"
-      ]
-    }
-  ]
-      '
-  bpftool_desired_output=$(echo "$bpftool_desired_output" | jq --sort-keys)
+  expected_path="${path:-/sys/fs/bpf}"
+  expected_cmds="$(echo "$cmds" | tr ',' '\n' | sort)"
+  expected_maps="$(echo "$maps" | tr ',' '\n' | sort)"
+  expected_progs="$(echo "$progs" | tr ',' '\n' | sort)"
+  expected_attachs="$(echo "$attachs" | tr ',' '\n' | sort)"
 
-  test "$bpftool_output" = "$bpftool_desired_output"
-}
+  got_path="$(echo "$bpftool_output" | jq '.[0].token_info')"
+  got_cmds="$(echo "$bpftool_output" | jq -r '.[0].allowed_cmds.[]' | sort)"
+  got_maps="$(echo "$bpftool_output" | jq -r '.[0].allowed_maps.[]' | sort)"
+  got_progs="$(echo "$bpftool_output" | jq -r '.[0].allowed_progs.[]' | sort)"
+  got_attachs="$(echo "$bpftool_output" | jq -r '.[0].allowed_attachs.[]' | sort)"
 
-test_container_bpf_token_path() {
-  set -e
-  incus config set foo security.bpffs.path=/mnt
-  # we need to enable one of the delegate settings to enable the token
-  set -e
-  incus config set foo security.bpffs.delegate_cmds=map_create
-  set -e
-  incus start foo
-  set -e
-  bpftool_output="$(incus exec foo -- bpftool --json token list  | jq --sort-keys)"
-  set -e
-  incus stop foo
-
-  bpftool_desired_output='
-  [
-    {
-      "token_info": "/mnt",
-      "allowed_cmds": [
-        "map_create"
-      ],
-      "allowed_maps": [],
-      "allowed_progs": [],
-      "allowed_attachs": []
-    }
-  ]
-      '
-  bpftool_desired_output=$(echo "$bpftool_desired_output" | jq --sort-keys)
-
-  test "$bpftool_output" = "$bpftool_desired_output"
+  test "$expected_path" = "$got_path"
+  test "$expected_cmds" = "$got_cmds"
+  test "$expected_maps" = "$got_maps"
+  test "$expected_progs" = "$got_progs"
+  test "$expected_attachs" = "$got_attachs"
 }
